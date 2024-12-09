a1efa5a6bcf2be085db39645d8f692d3cbf73e8b
lakhanprajapat1 <lakhan10209@gmail.com>
2024-12-09 12:39:43 +0530

    add new

---
+++ scrollText-Animation/script.js
@@ -0,0 +1,31 @@
+
+
+function marqueAnimation(){
+    window.addEventListener("wheel", (dets)=>{
+        if (dets.deltaY<0) {
+            gsap.to("#marque",{
+                transform:"translateX(-200%)",
+                duration:4,
+                repeat:-1,
+                ease:"none"
+            })
+            gsap.to("#marque img",{
+                rotate:'180',
+    
+            })
+            
+        } else {
+            gsap.to("#marque",{
+                transform:"translateX(00%)",
+                duration:4,
+                repeat:-1,
+                ease:"none"
+            })
+            gsap.to("#marque img",{
+                rotate:'0'
+            })
+        }
+    })
+}
+
+marqueAnimation()
